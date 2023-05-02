deef48dadc13cf7184658d5311f646fa493381fb
radhgawande5050 <radhgawande5050@gmail.com>
2023-05-02 18:26:44 +0530

    second commit

--- src/main/java/com/calculator/controller/FirstController.java
+++ src/main/java/com/calculator/controller/FirstController.java
@@ -1,5 +1,12 @@
 package com.calculator.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class FirstController {
+	@GetMapping("/Greeting")
+	public String welcome() {
+		return "Greeting";
+		
+	}
 
 }
